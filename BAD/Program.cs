20c8f24726574f6118be20a8fbc5a24fb13a6bb5
MarcuzziFranco <marcuzzi7franco@gmail.com>
2024-01-12 08:08:38 -0300

    feat: update programs

--- BAD/Program.cs
+++ BAD/Program.cs
@@ -9,18 +9,22 @@
 * 7- Read a Json with an array object and generate values. ✔️
 * 8- Read a Json from a CSV file with only values.                              !!!Low priority!!!
 * 9- Be able to determine which values change and which do not by a CSV file.   !!!Low priority!!!
-* 10- Execute a GET request towards a service with a generated Json (one case).
+* 10- Execute a GET request towards a service with a generated Json (one case). ✔️
 * 11- Execute GET requests towards a service with multiple generated Json.
 * 12- Save the result of the request and the service response.
-* 13- Generate a request using CURL and load a Json.
+* 13- Generate a request using CURL and load a Json.                            !!!Low priority!!!
+* 14- Generate a new json with null value. ✔️ (support in feat 15)
+* 15- Generate a new json with default value. ✔️
+* 16- Setting diccionary with default value
+* 17- Execute POST request towards a services with a generate Json ✔️ (lack test)
  * ***/
 
 using BAD.Generator;
 using BAD.JsonReader;
+using BAD.Services;
 
-
-string pathJson = "C:\\Users\\Franc\\PERSONAL\\NET\\BAD\\BAD\\jsonfiles\\test1.json";
-int count = 5;
+string pathJson = "C:\\Users\\Franc\\PERSONAL\\NET\\BAD\\BAD\\jsonfiles\\test3.json";
+int count = 1;
 
 var json = File.ReadAllText(pathJson);
 
@@ -38,6 +42,8 @@ for (int i = 0; i < count; i++)
 foreach (var item in listJsonGenerate)
 {
     Console.WriteLine(item);
+    dynamic response = await ExternalResources.PostAsync<dynamic, dynamic>("https://jsonplaceholder.typicode.com/posts", item);
+    Console.WriteLine(response);
 }
 
 
